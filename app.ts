df1cac01b1ee400b4cca96d08b29093ae9f5fe4a
AswinCG <cgaswin333@gmail.com>
2023-11-21 21:55:36 +0530

    feat: adding docs , bug fix required

--- app.ts
+++ app.ts
@@ -5,10 +5,19 @@ import helmet from "helmet";
 import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
 import morgan from "morgan";
 import { seedQuestions } from "./src/seeds/questionSeed";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
+
+//loading the yaml file
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 
 dotenv.config();
 
 const app: Application = express();
+
+//swagger documentation middlesware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(
 	cors({
@@ -16,6 +25,8 @@ app.use(
 		credentials: true,
 	} as CorsOptions)
 );
+
+
 
 app.use(helmet());
 app.use(express.json({ limit: "16kb" }));
